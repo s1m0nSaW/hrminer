9def34cdd63611ac0df77c0bd67f57c7c684169b
s1m0nSaW <simonsaw777@gmail.com>
2023-04-28 12:38:32 +0900

    fixed clientIp

--- utils/checkIp.js
+++ utils/checkIp.js
@@ -1,7 +1,8 @@
 // middleware для проверки IP адресов
 export default (req, res, next) => {
     const trustedIps = ['185.71.76.0/27','185.71.77.0/27','77.75.153.0/25','77.75.156.11','77.75.156.35','77.75.154.128/25','2a02:5180::/32']; // список доверенных IP адресов
-    const clientIp = req.ip; // получаем IP адрес клиента из запроса
+     // получаем IP адрес клиента из запроса
+    const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     if (trustedIps.indexOf(clientIp) !== -1) {
         console.log('ip адрес проверка прошел')
         // IP адрес клиента в списке доверенных, продолжаем выполнение
